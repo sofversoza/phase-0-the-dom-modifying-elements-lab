5c23f4925aff0c3c6c9e0206d6bb03d9399d2d54
sofiaversoza <sofversoza@gmail.com>
2021-11-12 16:40:42 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+// document.body.append(newHeader);
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'Sofia is the champion';
+
+
